7dad483d4f0cad85105bdf11ea583e8a7ad8fa93
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-08 18:06:52 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "parking.h"
+
+
+
+void main() {
+    print();
+    //int userInput;
+    //Parking parking;
+    //getParking(&parking);
+    //printParking(p);
+    //addParking("parking.txt", parking);
+
+}
+
