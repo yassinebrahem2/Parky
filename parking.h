a3bc43e30ba4ec70f02529848ab92678c4981098
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-08 18:06:59 +0100

    Create parking.h

---
+++ parking.h
@@ -0,0 +1,30 @@
+
+#ifndef parking_h
+#define parking_h
+
+#include <stdio.h>
+#include <string.h>
+
+
+// typedef struct {
+//     char ID[8];
+//     int numberOfSpots;
+//     char address[20];
+//     char municipality[20];
+//     float price;
+//     char agentCIN[10];
+//     int vehicules[4];
+//     int hasElectricCharger;
+// } Parking;
+
+
+//void getParkingNumber(char dir[], int* n);
+//void getParking(Parking *parking);
+//void printParking(Parking parking);
+//char *getVehiculesCode(char *vehicules[4]);
+//void addParking(char dir[], Parking newParking);
+//void deleteParking(int IDparking);
+//void modifyParking(Parking modifiedParking);
+//void sortParking(char attribute);
+void print();
+#endif
