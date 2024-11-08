00bc6f32468bfe237f08ee88265a574fc40d856f
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-08 19:59:03 +0100

    Update parking.c

--- parking.c
+++ parking.c
@@ -37,9 +37,9 @@ void getParking(Parking *parking) {
 */
 
 void printParking(Parking parking) {
-/*
     int n, i = 0;
     strcpy("123", parking.ID);
+/*
     parking.numberOfSpots = 10;
     strcpy("adres", parking.address);
     strcpy("munic", parking.municipality);
