85ab3161b87b6892c2c7802f20f9d757374b724c
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-10 14:35:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,26 @@
 
 
 void main() {
-    print();
+
+
     Parking parking;
+    strcpy(parking.ID, "123");
+    parking.numberOfSpots = 10;
+    strcpy(parking.address, "adres");
+    strcpy(parking.municipality, "munic");
+    parking.price = 100;
+    strcpy( parking.agentCIN, "idagent69");
+    parking.hasElectricCharger = 1;
+    parking.vehicules[0] = 1;
+    parking.vehicules[1] = 1;
+    parking.vehicules[2] = 1;
+    parking.vehicules[3] = 1;
+
     printParking(parking);
     //int userInput;
     //getParking(&parking);
     //printParking(p);
-    //addParking("parking.txt", parking);
+    addParking("parking.txt", parking);
 
 }
 
