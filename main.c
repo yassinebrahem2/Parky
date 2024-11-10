41fa963feb5ae43530421702263b010a9c3a8adb
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-10 19:55:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ void main() {
     parking.vehicules[3] = 1;
 
     //int userInput;
-    getParkingNumber(dir, &n);
+    getParkingNumber("Parking.txt", &n);
     printf("\n\n%d\n\n", n);
     //addParking("parking.txt", parking);
     // printParkings(dir);
