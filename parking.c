b8159b78fe907dec1c1ac36e3d4668174d782246
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-10 16:09:55 +0100

    Update parking.c

--- parking.c
+++ parking.c
@@ -103,7 +103,10 @@ void printParkings(char dir[]) {
         &parking.price, parking.agentCIN,
         vehiculeCode, &parking.hasElectricCharger) != EOF) {
         printf("VEHICULE CODE:%s\n", vehiculeCode);
-        getVehicules(parking.vehicules, vehiculeCode);
+        for (int i = 0; i < 4; i++) {
+            parking.vehicules[i] = vehiculeCode[i] - '0';
+        }
+        //getVehicules(parking.vehicules, vehiculeCode);
         printParking(parking);
     }
     fclose(parkingFile);
@@ -112,7 +115,7 @@ void printParkings(char dir[]) {
 void getVehicules(int vehicules[4], char vehiculeCode[4]) {
     int i;
     for(i = 0; i < 4; i++) {
-        if (strcmp(vehiculeCode[i], "1") == 0) {
+        if (strcmp(&vehiculeCode[i], "1") == 0) {
             vehicules[i] = 1;
         }
     }
