519b018e138b3b5f1e3ef07fe70050cab656583d
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-10 19:41:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,28 +6,28 @@
 
 void main() {
     printf("HELLOOOO");
-    int n;
-    Parking parking;
-    char dir[] = "Parking.txt";
-    strcpy(parking.ID, "123");
-    parking.numberOfSpots = 10;
-    strcpy(parking.address, "adres");
-    strcpy(parking.municipality, "munic");
-    parking.price = 50;
-    strcpy( parking.agentCIN, "idagent69");
-    parking.hasElectricCharger = 1;
-    parking.vehicules[0] = 1;
-    parking.vehicules[1] = 1;
-    parking.vehicules[2] = 1;
-    parking.vehicules[3] = 1;
+    // int n;
+    // Parking parking;
+    // char dir[] = "Parking.txt";
+    // strcpy(parking.ID, "123");
+    // parking.numberOfSpots = 10;
+    // strcpy(parking.address, "adres");
+    // strcpy(parking.municipality, "munic");
+    // parking.price = 50;
+    // strcpy( parking.agentCIN, "idagent69");
+    // parking.hasElectricCharger = 1;
+    // parking.vehicules[0] = 1;
+    // parking.vehicules[1] = 1;
+    // parking.vehicules[2] = 1;
+    // parking.vehicules[3] = 1;
 
-    //int userInput;
-    getParkingNumber("parking.txt", &n);
-    printf("\n\n%d\n\n", n);
-    //addParking("parking.txt", parking);
-    printParkings(dir);
-    //deleteParking(dir, "3");
-    //modifyParking(dir, parking);
+    // //int userInput;
+    // getParkingNumber("parking.txt", &n);
+    // printf("\n\n%d\n\n", n);
+    // //addParking("parking.txt", parking);
+    // printParkings(dir);
+    // //deleteParking(dir, "3");
+    // //modifyParking(dir, parking);
 
 }
 
