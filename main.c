ac9432c4d16ddce5210363954906dabca9d2b9b3
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-11 19:05:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 void main() {
     int n;
     Parking parking;
-    Parking parking1;
     char *dir = "Parking.txt";
     strcpy(parking.ID, "123");
     parking.numberOfSpots = 10;
@@ -21,33 +20,15 @@ void main() {
     parking.vehicules[2] = 1;
     parking.vehicules[3] = 1;
 
-    strcpy(parking1.ID, "10");
-    parking1.numberOfSpots = 10;
-    strcpy(parking1.address, "adres");
-    strcpy(parking1.municipality, "munic");
-    parking1.price = 6969;
-    strcpy( parking1.agentCIN, "idagent69");
-    parking1.hasElectricCharger = 1;
-    parking1.vehicules[0] = 1;
-    parking1.vehicules[1] = 1;
-    parking1.vehicules[2] = 1;
-    parking1.vehicules[3] = 1;
-
     //int userInput;
-    //getParkingNumber(dir, &n);
+    getParkingNumber(dir, &n);
     printf("\n\n%d\n\n", n);
 
-    displayParking(parking);
-    displayParking(parking1);
-    printf("\n\n\n");
-    swapParking(&parking, &parking1);
-    displayParking(parking);
-    displayParking(parking1);
     //addParking(dir, parking);
     // printParkings(dir);
     //deleteParking(dir, "3");
     //modifyParking(dir, parking);
-    //sortParking(dir);
+    sortParking(dir);
 
     //createSortFile(dir);
 
