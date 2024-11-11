b066eb7013c111bb900cabf7302526ca9b2fd178
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-11 19:37:34 +0100

    Update parking.h

--- parking.h
+++ parking.h
@@ -28,7 +28,8 @@ void addParking(char *dir, Parking parking);
 void deleteParking(char *dir, char IDparking[8]);
 void modifyParking(char *dir, Parking modifiedParking);
 void createSortFile(char *dir);
-void sortParking(char *dir);
+int price(Parking parking1, Parking parking2);
+void sortParking(char *dir, int (*compare)(Parking, Parking));
 void swapParking(Parking *parking1, Parking *parking2);
 
 #endif
