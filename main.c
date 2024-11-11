8997dd0718a2795a9da90da2c9f95d1d930818fb
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-11 19:48:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ void main() {
     // printParkings(dir);
     //deleteParking(dir, "3");
     //modifyParking(dir, parking);
-    sortParking(dir, price);
+    sortParking(dir, price, 1);
 
     //createSortFile(dir);
 
