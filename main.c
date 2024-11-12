d505a26aec5e76649eb9317c41363f703d9c8654
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-12 10:03:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ void main() {
     getParkingNumber(dir, &n);
     printf("\n\n%d\n\n", n);
 
-    //addParking(dir, parking);
+    addParking(dir, parking);
     // printParkings(dir);
     //deleteParking(dir, "3");
     //modifyParking(dir, parking);
