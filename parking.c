832f1bb586002dc4ed4c6e1358a1a3e76c3c59b9
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-12 11:05:58 +0100

    Update parking.c

--- parking.c
+++ parking.c
@@ -297,14 +297,14 @@ int validString(char *string1, char *string2) {
     return 1;
 }
 
-// int scanAgent(char *filename, Agent *agent) {
-//     FILE *f = fopen(filename, "a");
-//     if (f != NULL) {
-//         fprintf(f, "%s %s %s %d %d %d %s %s %d %d %d %d %d %d %d %d %d %s %s\n",
-//                 agent->cin, agent->nom, agent->prenom, agent->date_naissance.jour, agent->date_naissance.mois,
-//                 agent->date_naissance.annee, agent->salaire, agent->adresse, &agent->sexe,
-//                 agent->services[0], agent->services[1], agent->services[2], agent->services[3], agent->services[4],
-//                 agent->services[5], agent->services[6], agent->etat, agent->id_parking, agent->numtel);
-//         fclose(f);
-//     }
-// }
+int scanAgent(char *filename, Agent *agent) {
+    FILE *f = fopen(filename, "a");
+    if (f != NULL) {
+        fprintf(f, "%s %s %s %d %d %d %s %s %d %d %d %d %d %d %d %d %d %s %s\n",
+                agent->cin, agent->nom, agent->prenom, agent->date_naissance.jour, agent->date_naissance.mois,
+                agent->date_naissance.annee, agent->salaire, agent->adresse, &agent->sexe,
+                agent->services[0], agent->services[1], agent->services[2], agent->services[3], agent->services[4],
+                agent->services[5], agent->services[6], agent->etat, agent->id_parking, agent->numtel);
+        fclose(f);
+    }
+}
