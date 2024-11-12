d2563da95de26c021365f6dfae7e1b6385f1890d
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-12 11:57:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ void main() {
     // printf("\nFILTERING...\n");
     FILE *agentFile = fopen("Agent.txt", "r");
     scanAgent(agentFile, &agent);
-    printf("\n%s\n", agent.cin);
+    printf("\n%s\n", agent.id_parking);
     fclose(agentFile);
     //filterAvailableAgents();
 
