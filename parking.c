411cd01beb23595f5d43bc38772f854ecf4ec5a8
Vo1dSpiRiT <79236444+Vo1dSpiRiT@users.noreply.github.com>
2024-11-12 11:58:10 +0100

    Update parking.c

--- parking.c
+++ parking.c
@@ -318,18 +318,13 @@ void filterAvailableAgents() {
     FILE *agentFile = fopen("Agent.txt", "r");
     FILE *filteredFile = fopen("filteredAgent.txt", "w");
     printf("started");
-    // while (scanAgent(filteredFile, &agent) != EOF) {
-    //     printf("%s", agent.cin);
-    //     if (agent.id_parking == "0") {
-    //         printAgent(filteredFile, agent);
-    //     }
-    // }
-    for(i = 0; i < 6; i++) {
+    while (scanAgent(agentFile, &agent) != EOF) {
         printf("%s", agent.cin);
         if (agent.id_parking == "0") {
             printAgent(filteredFile, agent);
         }
     }
+
     printf("\nDONE\n");
 
     fclose(agentFile);
